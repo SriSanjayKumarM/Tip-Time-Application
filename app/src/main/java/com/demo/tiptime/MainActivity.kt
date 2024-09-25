655e6869e9cb0846026097d321b91016c5f506fd
SriSanjayKumarM <srisanjaykumar2000@gmail.com>
2024-09-25 09:38:07 +0530

    Added Local Tests

--- app/src/main/java/com/demo/tiptime/MainActivity.kt
+++ app/src/main/java/com/demo/tiptime/MainActivity.kt
@@ -164,7 +164,7 @@ fun RoundTheTipRow(
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
     var tip = tipPercent / 100 * amount
     if (roundUp) {
         tip = kotlin.math.ceil(tip)
